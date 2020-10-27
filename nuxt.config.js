561540dbddf92c9ddc96b9ed68636e157e6a80cb
ButlerFuqua <butlerfuqua@gmail.com>
2020-10-27 19:51:04 -0400

    remove vercel config

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,27 +82,6 @@ export default {
     ],
   ],
 
-  builds: [
-    {
-      src: "nuxt.config.js",
-      use: "@nuxtjs/now-builder",
-      config: {
-        serverFiles: ["package.json"]
-      }
-    }
-  ],
-  routes: [
-    { src: "/_nuxt/.+", headers: { "Cache-Control": "max-age=31557600" } },
-    {
-      src: "/sw.js",
-      dest: "/_nuxt/static/sw.js",
-      headers: {
-        "cache-control": "public, max-age=43200, immutable",
-        "Service-Worker-Allowed": "/"
-      }
-    },
-    { src: "/(.*)", dest: "/" }
-  ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
