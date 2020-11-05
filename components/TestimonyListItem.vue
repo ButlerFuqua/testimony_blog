3f932c83a8db8d250739e91bbad310a78abec593
ButlerFuqua <butlerfuqua@gmail.com>
2020-11-04 19:44:26 -0500

    Turn tags back to span with text

--- components/TestimonyListItem.vue
+++ components/TestimonyListItem.vue
@@ -10,10 +10,10 @@
     >
     <p>{{ testimony.description }}</p>
     <div class="tags">
-      <!-- <span :key="tag" v-for="tag in testimony.tags">{{ tag }} </span> -->
-      <div :key="tag" v-for="tag in testimony.tags">
+      <span :key="tag" v-for="tag in testimony.tags">{{ tag }} </span>
+      <!-- <div :key="tag" v-for="tag in testimony.tags">
         <img :src="`app-icons/${tag}_icon.svg`" :alt="`${tag} icon`" />
-      </div>
+      </div> -->
     </div>
     <br />
     <div class="readBtnContainer">
@@ -64,38 +64,38 @@ small {
 
 .tags {
   display: flex;
-  // span {
-  //   padding: 0.1rem 0.5rem;
-  //   border: 1px solid #f0c002;
-  //   background: #f0c002;
-  //   border-radius: 2rem;
-  //   margin: 0.5rem 0;
-  //   font-size: 0.8rem;
-  // }
-  // span + span,
-  // img + img {
-  //   margin-left: 0.5rem;
-  // }
-
-  div {
-    width: 45px;
-    height: 45px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: 2px solid #2aa1d4;
-    border-radius: 45px;
+  span {
+    padding: 0.1rem 0.5rem;
+    border: 1px solid #f0c002;
+    background: #f0c002;
+    border-radius: 2rem;
     margin: 0.5rem 0;
+    font-size: 0.8rem;
   }
-  div + div {
+  span + span,
+  img + img {
     margin-left: 0.5rem;
   }
 
-  img {
-    width: 30px;
-    max-height: 30px;
-    padding: 1px;
-  }
+  // div {
+  //   width: 45px;
+  //   height: 45px;
+  //   display: flex;
+  //   justify-content: center;
+  //   align-items: center;
+  //   border: 2px solid #2aa1d4;
+  //   border-radius: 45px;
+  //   margin: 0.5rem 0;
+  // }
+  // div + div {
+  //   margin-left: 0.5rem;
+  // }
+
+  // img {
+  //   width: 30px;
+  //   max-height: 30px;
+  //   padding: 1px;
+  // }
 }
 
 .readBtnContainer {
