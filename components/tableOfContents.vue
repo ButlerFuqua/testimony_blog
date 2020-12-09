ccb1cade2fb0df0ec48b680b3e8dc2347c066bc5
ButlerFuqua <butlerfuqua@gmail.com>
2020-12-09 15:23:18 -0500

    Make TOC component dynamic

--- components/tableOfContents.vue
+++ components/tableOfContents.vue
@@ -1,17 +1,24 @@
 <template>
     <div>
-          <h2>Table of contents</h2>
+        <h2>Table of contents</h2>
         <v-list>
           <v-list-item-group v-model="model">
             <v-list-item
-              v-for="belief in beliefs" :key="belief.title"
-              :href="`#${belief.title.replace(/\s/g, '_')}`"
+              v-for="item in items" :key="item.title"
+              :href="`#${item.title.replace(/\s/g, '_')}`"
             >
               <v-list-item-content>
-                <v-list-item-title  v-text="belief.title"></v-list-item-title>
+                <v-list-item-title  v-text="item.title"></v-list-item-title>
               </v-list-item-content>
             </v-list-item>
           </v-list-item-group>
         </v-list>
     </div>
 </template>
+
+<script>
+export default {
+    name: "TableOfContents",
+    props: ["items"]
+}
+</script>
