5d390b794c49863268c2f80105eeb07c58a11936
ButlerFuqua <butlerfuqua@gmail.com>
2020-12-15 19:42:07 -0500

    Update about me section

--- components/aboutButler.vue
+++ components/aboutButler.vue
@@ -2,15 +2,13 @@
   <div>
     <h2 class="headline">About Butler Fuqua</h2>
     <br />
-    <p>I'm follwer of Jesus Christ and a full-time software engineer.</p>
+    <p>Hi, I'm Butler. Thank you for visiting my website.</p>
     <p>
-      I am the one that built this website, and I am building the Testimonies
-      App in my spare time.
+      I'm a follwer of Jesus Christ and a full-time software engineer. This
+      project is something I feel very strongly about, and I am excited to see
+      what the Lord does with it!
     </p>
-    <p>
-      I truely believe in this project, and I am excited to see what the Lord is
-      going to do with it!
-    </p>
+    <p>Please consider signing up if you haven't already done so.</p>
   </div>
 </template>
 
